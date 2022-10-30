8b6fb994ec335a38356a0bad878d5ccb211be561
Ashley <ashley.pergoliti@gmail.com>
2022-10-30 23:23:18 +1000

    Adding the basic folder and file structure for the main game

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,2 @@
 const W=1000;
 const H=600;
-
-const KEYS={
-    W:61,
-    A:65,
-    D:68
-}
--- js/main.js
+++ js/main.js
@@ -1,24 +1,11 @@
 "use strict";
 
-let player = new Player();
-let balls = new BallManager();
-let level = new LevelManager();
-
 function preload() {}
 
 function setup() {
   createCanvas(W, H);
-  player.setup();
-  level.setup();
-  balls.setup();
-  textAlign(RIGHT);
-  console.log('testing a change')
+
 }
 function draw() {
-  player.draw(balls);
-  level.draw(balls);
-  balls.draw();
-
   background("grey");
-  drawSprites();
 }
