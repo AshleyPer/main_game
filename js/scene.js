d24d01e449f796baf075293288059eee7a1d2132
Jodie Thomson <thecurlypurpleshoe@gmail.com>
2022-10-31 21:58:02 +1000

    add scene sketch

---
+++ js/scene.js
@@ -0,0 +1,10 @@
+function testScene(){
+    fill(0,0,255)
+    rectMode(CENTER);
+    rect(width/2, height/2, 200,200);
+    stroke(255)
+    textAlign(CENTER)
+    textSize(30)
+    text("Scene Test", width/2, height/2);
+
+}
