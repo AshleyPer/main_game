619d50749c1d05e06f6e9b0a7c115bdbef7c08d0
Cameron Bowman <periphery22@hotmail.com>
2022-11-01 17:38:49 +1100

    added screen state switching

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ let bricks1;
 
 let ninja = new Ninja(W / 2, H / 2);
 
+let screenState = 0;
+const LOADING = 0;
+const MAIN_MENU = 1;
+const GAMEPLAY = 2;
+const GAME_OVER = 3;
+
+
 
 function preload() {
  
@@ -17,11 +24,40 @@ function setup() {
     
 }
 function draw() {
+  switch(screenState){
+    case 0:
+      drawLoading();
+      break;
+    case 1:
+      drawMainMenu();
+      break;
+    case 2:
+      drawGameplay();
+      break;
+    case 3:
+      drawGameover();
+      break;
+  }
+  
+}
+
+function drawLoading() {
+
+}
+
+function drawMainMenu() {
+  
+}
+
+function drawGameplay() {
   background("grey");
   testScene();
 
   drawSprites();
     
   ninja.attackAnimation();
+}
+
+function drawGameover() {
   
 }
