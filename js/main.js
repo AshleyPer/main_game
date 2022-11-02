a30ef44834a16ba0218c2fb7290a3f86d7be4c26
Jodie Thomson <thecurlypurpleshoe@gmail.com>
2022-11-02 15:00:54 +1000

    added character pop up and image functions for main menu

--- js/main.js
+++ js/main.js
@@ -77,24 +77,11 @@ function drawMainMenu() {
   //this is test code for character info
     //this may stay on main menu(if we use) 
       //or a new screen will make for it that's accessed from Main menu
-  image(ninTest, 30,170);
-  rectMode(CENTER);
-  fill(100);
-  rect(920,70,95,80);//sample space  - image to replace
-  rect(920,170,65,80);
-  rect(920,270,65,80);
-  rect(920,370,65,80);
-  rect(920,470,65,80);
-  fill(0);
-  noStroke();
-  strokeWeight(2);
-  textSize(14)
-  textStyle(BOLD);
-  text("Boss Enemy", 920,70);
-  text("Enemy 1", 920,170);
-  text("Enemy 2", 920,270);
-  text("Enemy 3", 920,370);
-  text("Enemy 4", 920,470);
+  characterImages();
+  
+  characterPopUps();
+
+ // text(mouseX + "," + mouseY, mouseX, mouseY);
   
   
 }
@@ -158,6 +145,106 @@ function exitBnPressed(){//takes you where? - may not use yet
 
 }
 
+function characterImages(){
+image(ninTest, 30,170);
+//replace the following code with enemy images
+rectMode(CENTER);
+fill(100);
+rect(920,70,95,80);//sample space  - image to replace
+rect(920,170,65,80);
+rect(920,270,65,80);
+rect(920,370,65,80);
+rect(920,470,65,80);
+fill(0);
+noStroke();
+strokeWeight(2);
+textSize(14)
+textStyle(BOLD);
+text("Boss Enemy", 920,70);
+text("Enemy 1", 920,170);
+text("Enemy 2", 920,270);
+text("Enemy 3", 920,370);
+text("Enemy 4", 920,470);
+}
+
+function characterPopUps(){//test code maybe use a function/class with parameters - could use a text file for text
+  if(mouseX >30 && mouseX < 125 && mouseY <430 && mouseY >170){//check position when final
+    stroke(120)
+    fill(100)
+    rect(200,200,150,150,30)
+    noStroke()
+    fill(0)
+    text("Hero Ninja", 200,160)
+    text("He/she has a sword", 200,180)//add specs
+    text("Move him with left", 200,200)
+    text("and right arrows", 200,220)
+    text("space bar on/off", 200,240)
+    text("for attack ", 200,260)
+  }
+  if(mouseX >880 && mouseX < 960 && mouseY <100 && mouseY >35){//check these once image up
+    stroke(120)
+    fill(100)
+    rect(800,65,110,80,30)
+    noStroke()
+    fill(0)
+    textSize(12)
+    text("Boss Enemy", 800,50)
+    text("He is bad", 800,70)// add specs
+    text("And kicks butt", 800,90)
+    //text("....", 800,110)
+   
+  }
+   
+  if(mouseX >890 && mouseX < 950 && mouseY <210 && mouseY >130){//check these once image up
+    stroke(120)
+    fill(100)
+    rect(800,170,110,80,30)
+    noStroke()
+    fill(0)
+    textSize(12)
+    text("Enemy 1", 800,150)
+    text("He/she is also bad", 800,170)//add specs
+    text("but is not boss", 800,190)
+    
+  }
+  if(mouseX >890 && mouseX < 950 && mouseY <300 && mouseY >235){//check these once image up
+    stroke(120)
+    fill(100)
+    rect(800,270,110,80,30)
+    noStroke()
+    fill(0)
+    textSize(12)
+    text("Enemy 2", 800,250)
+    text("He/she is also bad", 800,270)// add specs
+    text("but is not boss", 800,290)
+    
+  }
+  if(mouseX >890 && mouseX < 950 && mouseY <400 && mouseY >330){//check these once image up
+    stroke(120)
+    fill(100)
+    rect(800,370,110,80,30)
+    noStroke()
+    fill(0)
+    textSize(12)
+    text("Enemy 3", 800,350)//add specs
+    text("He/she is also bad", 800,370)
+    text("but is not boss", 800,390)
+    
+  }
+  if(mouseX >890 && mouseX < 950 && mouseY <500 && mouseY >430){//check these once image up
+    stroke(120)
+    fill(100)
+    rect(800,470,110,80,30)
+    noStroke()
+    fill(0)
+    textSize(12)
+    text("Enemy 4", 800,450)
+    text("He/she is also bad", 800,470)//add specs
+    text("but is not boss", 800,490)
+    
+  }
+  
+}
 
 function hideAllBns(){
   mainMenuBn.hide();
