45c67401531deea45581e44557ae8bd3841af390
Jodie Thomson <thecurlypurpleshoe@gmail.com>
2022-11-04 23:52:13 +1000

    minor main menu update

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let mainMenuBn, gamePlayBn, exitBn, leaderBoardBn;//buttons
 let ninTest;
 let ninjaLoadingScreen;
 let nls;
-let hero, redN, redN2;//spec pics
+let hero, E1a, E1b,E2a, E2b,E3a, E3b, Bea, Beb;//spec pics
 
 let x1 = 0;
 let x2;
@@ -27,8 +27,14 @@ function preload() {
   ninTest = loadImage("assets/ninjapictest.png");//pic for character test main menu
   ninjaLoadingScreen = loadImage('assets/img/ninja_player/attack/attack_east/attack_3.png');
   hero = loadImage('assets/img/ninja_player/attack/attack_east/attack_2.png');
-  redN = loadImage('assets/img/enemies/tier1_red_guy/run/run1.png');
-  redN2 = loadImage('assets/img/enemies/tier1_red_guy/attack/attack3.png');
+  E1a = loadImage('assets/img/enemies/tier1_red_guy/run/run1.png');
+  E1b = loadImage('assets/img/enemies/tier1_red_guy/attack/attack3.png');
+  E2a = loadImage('assets/img/enemies/tier2_red_ninja/run/run1.png');
+  E2b = loadImage('assets/img/enemies/tier2_red_ninja/attack/attack3.png');
+  E3a = loadImage('assets/img/enemies/tier3_red_orange_ninja/run/run1.png');
+  E3b = loadImage('assets/img/enemies/tier3_red_orange_ninja/attack/attack3.png');
+  Bea = loadImage('assets/img/enemies/boss/run/run2.png');
+  Beb = loadImage('assets/img/enemies/boss/run/run3.png');
 }
 
 function setup() {
@@ -111,19 +117,15 @@ function drawMainMenu() {
   stroke(150);
   text("Main Menu", width / 2, height / 2 - 100);
   text("Under Construction", width / 2, height / 2 - 50);
-  text("Adding Art", width / 2, height / 2);
-  text("Maybe links to Character info", width / 2, height / 2 + 50);
-  text("other stuff inc maybe levels tbc", width / 2, height / 2 + 100);
+  
   textSize(16)
   text("Hover Mouse over Characters for Specs", width / 2 - 340, height / 2 + 220);
-  //this is test code for character info
-  //this may stay on main menu(if we use) 
-  //or a new screen will make for it that's accessed from Main menu
+ 
   characterImages();
 
   characterPopUps();
 
-  // text(mouseX + "," + mouseY, mouseX, mouseY);
+ //text(mouseX + "," + mouseY, mouseX, mouseY);
 
 
 }
@@ -189,108 +191,98 @@ function exitBnPressed() {//takes you where? - may not use yet
 
 function characterImages() {
   image(ninTest, 60, 170);
-  redN2.resize(0,60)
-  image(redN2,890,145)
-  //replace the following code with enemy images
+  Bea.resize(0,60);
+  image(Bea,885,60);
+  E3b.resize(0,60);
+  image(E3b,890,190);
+  E2b.resize(0,60);
+  image(E2b,890,310);
+  E1b.resize(0,60);
+  image(E1b,890,430);
+  
+}
+
+function characterPopUps() {//test code maybe use a function/class with parameters - could use a text file for text
   rectMode(CENTER);
   fill(100);
-  rect(920, 70, 95, 80);//sample space  - image to replace
-  //rect(920, 170, 65, 80);
-  rect(920, 270, 65, 80);
-  rect(920, 370, 65, 80);
-  rect(920, 470, 65, 80);
   fill(0);
   noStroke();
   strokeWeight(2);
   textSize(14)
   textStyle(BOLD);
-  text("Boss Enemy", 920, 70);
- // text("Red Ninja", 920, 170);
-  text("Enemy 2", 920, 270);
-  text("Enemy 3", 920, 370);
-  text("Enemy 4", 920, 470);
-}
-
-function characterPopUps() {//test code maybe use a function/class with parameters - could use a text file for text
   if (mouseX > 30 && mouseX < 125 && mouseY < 430 && mouseY > 170) {//check position when final
-    stroke(120)
-    fill(100)
-    rect(230, 200, 150, 150, 30)
-    noStroke()
-    fill(0)
-    hero.resize(0,35)
-    image(hero,180,132)
-    text("Hero Ninja", 245, 154)
-    text("UPDATE INFO", 230, 185)//add specs
-    text("Move him with left", 230, 205)
-    text("and right arrows", 230, 225)
-    text("space bar for attack", 230, 245)
-    text(" ", 230, 265)
+    stroke(120);
+    fill(100);
+    rect(230, 200, 150, 150, 30);
+    noStroke();
+    fill(0);
+    hero.resize(0,35);
+    image(hero,180,132);
+    text("Hero Ninja", 245, 154);
+    text("UPDATE INFO", 230, 185);//add specs
+    text("Move him with left", 230, 205);
+    text("and right arrows", 230, 225);
+    text("space bar for attack", 230, 245);
+    text(" ", 230, 265);
   }
-  if (mouseX > 880 && mouseX < 960 && mouseY < 100 && mouseY > 35) {//check these once image up
-    stroke(120)
-    fill(100)
-    rect(800, 65, 110, 80, 30)
-    noStroke()
-    fill(0)
-    textSize(12)
-    text("Boss Enemy", 800, 50)
-    text("He is bad", 800, 70)// add specs
-    text("And kicks butt", 800, 90)
-    //text("....", 800,110)
+  if (mouseX > 890 && mouseX < 950 && mouseY < 135 && mouseY > 50) {//check these once image up
+    stroke(120);
+    fill(100);
+    rect(800, 95, 120, 80, 30);
+    Beb.resize(0,30);
+    image(Beb,740,80);
+    noStroke();
+    fill(0);
+    textSize(12);
+    text("Boss Enemy", 800, 80);
+    text("Weapon:", 800, 100);// add specs
+    text("Strikes to Kill:", 800, 120);
+    //text("....", 800,110);
 
   }
 
-  if (mouseX > 890 && mouseX < 950 && mouseY < 210 && mouseY > 130) {//check these once image up
-    stroke(120)
-    fill(100)
-    rect(800, 170, 110, 80, 30)
-    redN.resize(0,30)
-    image(redN,750,150)
-    noStroke()
-    fill(0)
-    textSize(12)
-    text("Red Ninja", 800, 150)
-    text("more info", 800, 170)//add specs
-    text(".......", 800, 190)
+  if (mouseX > 890 && mouseX < 930 && mouseY < 250 && mouseY > 190) {//check these once image up
+    stroke(120);
+    fill(100);
+    rect(800, 220, 120, 80, 30);
+    E3a.resize(0,30);
+    image(E3a,750,200);
+    noStroke();
+    fill(0);
+    textSize(12);
+    text("Name:", 800, 200);
+    text("Weapon:", 800, 220);//add specs
+    text("Strikes to kill:", 800, 240);
+  }
+  if (mouseX > 890 && mouseX < 930 && mouseY < 350 && mouseY > 285) {//check these once image up
+    stroke(120);
+    fill(100);
+    rect(800, 340, 120, 80, 30);
+    E2a.resize(0,30);
+    image(E2a,750,320);
+    noStroke();
+    fill(0);
+    textSize(12);
+    text("Name:", 800, 320);
+    text("Weapon: ", 800, 340);// add specs
+    text("Strokes to kill: ", 800, 360);
 
   }
-  if (mouseX > 890 && mouseX < 950 && mouseY < 300 && mouseY > 235) {//check these once image up
-    stroke(120)
-    fill(100)
-    rect(800, 270, 110, 80, 30)
-    noStroke()
-    fill(0)
-    textSize(12)
-    text("Enemy 2", 800, 250)
-    text("He/she is also bad", 800, 270)// add specs
-    text("but is not boss", 800, 290)
+  if (mouseX > 890 && mouseX < 930 && mouseY < 490 && mouseY > 430) {//check these once image up
+    stroke(120);
+    fill(100);
+    rect(800, 465, 120, 80, 30);
+    E1a.resize(0,30);
+    image(E1a,750,450);
+    noStroke();
+    fill(0);
+    textSize(12);
+    text("  Name:", 800, 450);//add specs
+    text("  Weapon: Fists", 800, 470);
+    text("  Strikes to Kill:", 800, 490);
 
   }
-  if (mouseX > 890 && mouseX < 950 && mouseY < 400 && mouseY > 330) {//check these once image up
-    stroke(120)
-    fill(100)
-    rect(800, 370, 110, 80, 30)
-    noStroke()
-    fill(0)
-    textSize(12)
-    text("Enemy 3", 800, 350)//add specs
-    text("He/she is also bad", 800, 370)
-    text("but is not boss", 800, 390)
-
-  }
-  if (mouseX > 890 && mouseX < 950 && mouseY < 500 && mouseY > 430) {//check these once image up
-    stroke(120)
-    fill(100)
-    rect(800, 470, 110, 80, 30)
-    noStroke()
-    fill(0)
-    textSize(12)
-    text("Enemy 4", 800, 450)
-    text("He/she is also bad", 800, 470)//add specs
-    text("but is not boss", 800, 490)
-
-  }
+  
 
 }
 
