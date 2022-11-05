537f4bef3b9f52ba2aac6912dc5587abd6859664
Jodie Thomson <thecurlypurpleshoe@gmail.com>
2022-11-06 02:27:02 +1000

    Enemy functions update

--- js/enemyfunctions.js
+++ js/enemyfunctions.js
@@ -1,77 +1,97 @@
-//load images - different for each - PARAMETER
-//load animation attack
-//load animation death
-
-//positions
-    //start position
-    //change postition
-    //change directions - due to ?
-
-//start life
-//reduce life
-//remove body
-
-//interact with player
-    //when enemy collide player - changeAnimation
-//interact with scene
-    //tbc
-
-//enemy group?
-
-
-// start of the enemy class
-   // basic function, one direction
-   //lots still to do
 class Enemy {
     constructor(positionX, positionY) {
         this.positionX = positionX;
         this.positionY = positionY;
-        this.attackEast;
-        this.runEast;
         this.enemySprite;
-       
-    }
-
-//different enemy animations - parameter?
-   loadEnemyAnimations() {
-    // test section - one enemy red ninja - one direction
         
-        this.attackEast = loadAnimation('assets/img/enemies/tier1_red_guy/attack/attack1.png', 'assets/img/enemies/tier1_red_guy/attack/attack6.png');
-        this.runEast = loadAnimation('assets/img/enemies/tier1_red_guy/run/run1.png', 'assets/img/enemies/tier1_red_guy/run/run4.png')
-      
     }
 
-    createEnemy() {
+   loadEnemyAnimations() {// all animations?
+   
+            
+        this.enemy1Attack = loadAnimation('assets/img/enemies/tier1_red_guy/attack/attack1.png', 'assets/img/enemies/tier1_red_guy/attack/attack6.png');
+        this.enemy1Run = loadAnimation('assets/img/enemies/tier1_red_guy/run/run1.png', 'assets/img/enemies/tier1_red_guy/run/run4.png');
+        this.enemy2Attack = loadAnimation('assets/img/enemies/tier2_red_ninja/attack/attack1.png', 'assets/img/enemies/tier2_red_ninja/attack/attack5.png');
+        this.enemy2Run = loadAnimation('assets/img/enemies/tier2_red_ninja/run/run1.png', 'assets/img/enemies/tier2_red_ninja/run/run4.png');
+        this.enemy3Attack = loadAnimation('assets/img/enemies/tier3_red_orange_ninja/attack/attack1.png', 'assets/img/enemies/tier3_red_orange_ninja/attack/attack5.png');
+        this.enemy3Run = loadAnimation('assets/img/enemies/tier3_red_orange_ninja/run/run1.png', 'assets/img/enemies/tier3_red_orange_ninja/run/run4.png');
+        this.enemyBossAttack = loadAnimation('assets/img/enemies/boss/attack/boss_attack1.png', 'assets/img/enemies/boss/attack/boss_attack5.png');
+        this.enemyBossRun = loadAnimation('assets/img/enemies/boss/run/run1.png', 'assets/img/enemies/boss/run/run4.png');
+        this.enemyBossDeath = loadAnimation('assets/img/enemies/boss/death/boss_death1.png', 'assets/img/enemies/boss/death/boss_death4.png');
+    }
+
+    createEnemy1() {
         this.enemySprite = createSprite(this.positionX, this.positionY, 40);
-        this.enemySprite.addAnimation('runEast', this.runEast);
-        this.enemySprite.addAnimation('attackEast', this.attackEast);
+        this.enemySprite.addAnimation('runEast1', this.enemy1Run);
+        this.enemySprite.addAnimation('attackEast1', this.enemy1Attack);
        
         this.enemySprite.debug = true;
         this.enemySprite.scale = 4;
         this.enemySprite.setSpeed(0.5,0);
 
     }
+
+    createEnemy2() {
+        this.enemySprite = createSprite(this.positionX, this.positionY, 40);
+        this.enemySprite.addAnimation('runEast2', this.enemy2Run);
+        this.enemySprite.addAnimation('attackEast2', this.enemy2Attack);
+       
+        this.enemySprite.debug = true;
+        this.enemySprite.scale = 4;
+        this.enemySprite.setSpeed(0.5,0);
+
+    }
+    createEnemy3() {
+        this.enemySprite = createSprite(this.positionX, this.positionY, 40);
+        this.enemySprite.addAnimation('runEast3', this.enemy3Run);
+        this.enemySprite.addAnimation('attackEast3', this.enemy3Attack);
+       
+        this.enemySprite.debug = true;
+        this.enemySprite.scale = 4;
+        this.enemySprite.setSpeed(0.5,0);
+
+    }
+    createEnemyB() {
+        this.enemySprite = createSprite(this.positionX, this.positionY, 40);
+        this.enemySprite.addAnimation('runEastB', this.enemyBossRun);
+        this.enemySprite.addAnimation('attackEastB', this.enemyBossAttack);
+        this.enemySprite.addAnimation('deathB', this.enemyBossDeath);
+        this.enemySprite.debug = true;
+        this.enemySprite.scale = 4;
+        this.enemySprite.setSpeed(0.5,0);
+
+    }
     move() {
+
+
         //due to interaction with scenery or player
        
       
     }
-
-    attackAnimation() {
-       // if the enemy collides with player - change animation(attackAnimation) with conditions
-
-
-        
-    }
-
-    life(){
-        //monitoring hits
-    }
-
-    death(){
-        //max hits causes death
-           // remove body after time
-    }
     
-}
+    attackAnimation1() {
+       // if the enemy collides with player - change animation(attackAnimation) with conditions
+      // if (dist(this.enemySprite.position.x, this.enemySprite.position.y, ninja.position.x,
+    this.enemySprite.changeAnimation('attackEast1');
+   
+    }
+    attackAnimation2() {
+        // if the enemy collides with player - change animation(attackAnimation) with conditions
+       // if (dist(this.enemySprite.position.x, this.enemySprite.position.y, ninja.position.x,
+     this.enemySprite.changeAnimation('attackEast2');
+    
+     }
+     attackAnimation3() {
+        // if the enemy collides with player - change animation(attackAnimation) with conditions
+       // if (dist(this.enemySprite.position.x, this.enemySprite.position.y, ninja.position.x,
+     this.enemySprite.changeAnimation('attackEast3');
+     
+     }
+     attackAnimationB() {
+        // if the enemy collides with player - change animation(attackAnimation) with conditions
+       // if (dist(this.enemySprite.position.x, this.enemySprite.position.y, ninja.position.x,
+     this.enemySprite.changeAnimation('attackEastB');
+     
+     }
 
+}
