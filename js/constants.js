b2f6d05949a347c1e2f0381ebbeecccd233fc39d
Jodie Thomson <thecurlypurpleshoe@gmail.com>
2022-11-12 23:04:25 +1000

    button

--- js/constants.js
+++ js/constants.js
@@ -11,5 +11,5 @@ const WINNER = 5;
 //buttonText, positionX, positionY, buttonWidth, buttonHeight, backgroundColour, fontSize
 const mainMenuBn = new Button('Main Menu', window.innerWidth / 2 - 60, H - 35, '130px', '45px', "blue", '20px');
 const gamePlayBn = new Button('Play Game', window.innerWidth / 2 - 60, H - 35, '130px', '45px', "blue", '20px');
-const exitBn = new Button('EXIT', window.innerWidth / 2 - 450, H - 15, '80px', '25px', "grey", '',);
+const exitBn = new Button('EXIT', window.innerWidth / 2 + 410, H - 15, '80px', '25px', "grey", '',);
 const leaderBoardBn = new Button('Leader Board', window.innerWidth / 2 + 350, H - 25, '100px', '35px', "blue", '');
